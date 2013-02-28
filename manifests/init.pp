45913e060685e860cd0b83d91a2d6a57baf2b766
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-02-28 21:53:08 +0100

    Added implementation for installing Adium

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install Adium to /Applications
+#
+# Examples
+#
+#  include adium
+class adium {
+  package { 'Adium':
+    provider => 'appdmg',
+    source   => 'http://download.adium.im/Adium_1.5.4.dmg',
+  }
+}
